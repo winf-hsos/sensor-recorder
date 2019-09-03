1dbf69b60fee4a613cd651acf1aa4cfc139f916f
Nicolas <nicolas.meseth@googlemail.com>
2019-09-03 17:48:00 +0200

    Enhanced the resulting object with min and max values

--- test.js
+++ test.js
@@ -3,15 +3,20 @@ var sensorRecorder = require('./index.js');
 var r = sensorRecorder.createSensorRecorder("Test Sensor");
 
 setTimeout(() => {
-    r.add(4.0);
-    console.log(r.getLatestValue());
-    var now = Date.now();
-    
-}, 4000);
-
-setTimeout(() => {
     var now = Date.now();
     console.log(r.getAvgValueForTimeInterval(now - 10000, now));
 }, 7000)
 
-r.add(1.0);
+addInMs(r, 1.0, 100);
+addInMs(r, 1.0, 460);
+addInMs(r, 1.01, 4000);
+addInMs(r, -10.3, 450);
+
+var start = Date.now();
+
+function addInMs(recorder, value, ms) {
+    setTimeout(() => { 
+        recorder.add(value);
+        console.log("Added >" + value + "< at time >" + (Date.now() - start) / 1000);
+    }, ms)
+}
